a1d5cebf7557eb2e19c981315490404c8d9375b5
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-07 12:52:01 +0330

    Decrypt Password Validation Developed

--- src/auth/password.js
+++ src/auth/password.js
@@ -1,9 +1,15 @@
 import bcrypt from "bcryptjs";
 
 export const hashPassword = async (password) => {
-  const saltRounds = parseInt(process.env.PASSWORD_SALT) || 10; 
+  const saltRounds = parseInt(process.env.PASSWORD_SALT) || 10;
   const salt = await bcrypt.genSalt(saltRounds);
   const hash = await bcrypt.hash(password, salt);
 
   return hash;
+};
+
+export const decryptPassword = async (hashedPassword, userEnteredPassword) => {
+  const password = await bcrypt.compare(userEnteredPassword, hashedPassword)
+
+  return password
 };
