7ffb041eb27f009d29c63cd9a7a4c47557fcf7c3
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-07 12:46:45 +0330

    Token Handler (Module) Renamed

---
+++ src/auth/token.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export default async function generateToken(parameter, expireAt) {
+  const securityCode = process.env.JWT_SECURITY;
+
+  const accessToken = jwt.sign(
+    {
+      phoneNumber: parameter,
+    },
+    securityCode,
+    {
+      expiresIn: expireAt,
+      algorithm: "HS256",
+    }
+  );
+
+  return accessToken;
+}
